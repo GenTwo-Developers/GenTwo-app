c86265ebf8894b32b224573735c4d6eef14ec646
RAINeko256 <reirain256@gmail.com>
2021-01-28 18:39:20 +0900

    feature:アイコンを配置

---
+++ src/js/views/components/block/control-bar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+//import "......../style/control-bar.css";
+import Bell from '../atoms/bell.js';
+import Edit from '../atoms/edit.js';
+import Home from '../atoms/home.js';
+import Search from '../atoms/search.js';
+
+
+class ControlBar extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="control-bar">
+                <Home/>
+                <Search/>
+                <Edit/>
+                <Bell/>
+            </div>
+        );
+    }
+
+}
+
+export default ControlBar;
