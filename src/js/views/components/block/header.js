f6bf0b520f56a3f5d6628a2c6a91308b9fa69f0d
tmtmNasubi <59120909+tmtmNasubi@users.noreply.github.com>
2021-02-22 00:38:59 +0900

    feat: header の作成（途中）．

---
+++ src/js/views/components/block/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'style/block/header.css';
+function Header(){
+    return(
+    <div className="header">
+    	<header>
+    		<h1 className="logo-header">GenTwo</h1>
+    	</header>
+
+    </div>
+    );
+}
+
+export default Header;
