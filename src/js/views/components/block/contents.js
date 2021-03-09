c3f3cf1ed5bea7be301632a230cf24f302e87fd0
RAINEKO256 <reirain256@gmail.com>
2021-03-09 18:12:56 +0900

    feat:メッセージ表示のブロックの作成
    material-uiパクった

--- src/js/views/components/block/contents.js
+++ src/js/views/components/block/contents.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'style/block/contents.css';
+import MsgBlock from 'js/views/components/block/msg-block';
 function Contents(){
-    return(
-        <div className="contents"></div>
-    );
+  return(
+    <div className="contents">
+      <MsgBlock />
+    </div>
+  );
 }
 
 export default Contents;
