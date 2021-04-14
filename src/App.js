57f0360825d1f185db5c9fea156d4ef56b8c1e10
tmtmNasubi <59120909+tmtmNasubi@users.noreply.github.com>
2021-04-14 16:58:51 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './style/template/App.css';
 import ControlBar from './js/views/components/block/control-bar.js';
 import Header from './js/views/components/block/header.js';
 import Contents from './js/views/components/block/contents.js';
+=======
+import logo from './logo.svg';
+import './App.css';
+import Header from './js/views/compornents/block/header';
+>>>>>>> Stashed changes
 
 function App() {
   return(
